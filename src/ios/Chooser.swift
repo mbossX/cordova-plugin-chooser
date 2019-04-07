6e4d6a2513bd1674389d479edecedb9cf814700f
1424667164 <1424667164@qq.com>
2019-04-07 22:52:50 +0800

    修改ios选择类型位图片视频

--- src/ios/Chooser.swift
+++ src/ios/Chooser.swift
@@ -6,51 +6,63 @@ import Foundation
 
 @objc(Chooser)
 class Chooser : CDVPlugin {
-	var commandCallback: String?
-
-	func callPicker (utis: [String]) {
-		let picker = UIDocumentPickerViewController(documentTypes: utis, in: .import)
-		picker.delegate = self
-		self.viewController.present(picker, animated: true, completion: nil)
-	}
-
-	func detectMimeType (_ url: URL) -> String {
-		if let uti = UTTypeCreatePreferredIdentifierForTag(
-			kUTTagClassFilenameExtension,
-			url.pathExtension as CFString,
-			nil
-		)?.takeRetainedValue() {
-			if let mimetype = UTTypeCopyPreferredTagWithClass(
-				uti,
-				kUTTagClassMIMEType
-			)?.takeRetainedValue() as? String {
-				return mimetype
-			}
-		}
-
-		return "application/octet-stream"
-	}
-
-	func documentWasSelected (url: URL) {
-		var error: NSError?
-
-		NSFileCoordinator().coordinate(
-			readingItemAt: url,
-			options: [],
-			error: &error
-		) { newURL in
-			let maybeData = try? Data(contentsOf: newURL, options: [])
-
-			guard let data = maybeData else {
-				self.sendError("Failed to fetch data.")
-				return
-			}
-			do {
+    var commandCallback: String?
+    
+    func callPicker (utis: [String]) {
+       if !UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            self.send("RESULT_CANCELED");
+            return;
+        }
+        let imagePicker = UIImagePickerController()
+        //设置代理
+        imagePicker.delegate = self
+        //指定图片控制器类型
+        imagePicker.sourceType = .photoLibrary
+        //只显示视频类型的文件
+        imagePicker.mediaTypes = utis
+        //不需要编辑
+        imagePicker.allowsEditing = false
+        //弹出控制器，显示界面
+        self.viewController.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func detectMimeType (_ url: URL) -> String {
+        if let uti = UTTypeCreatePreferredIdentifierForTag(
+            kUTTagClassFilenameExtension,
+            url.pathExtension as CFString,
+            nil
+            )?.takeRetainedValue() {
+            if let mimetype = UTTypeCopyPreferredTagWithClass(
+                uti,
+                kUTTagClassMIMEType
+                )?.takeRetainedValue() as String? {
+                return mimetype
+            }
+        }
+        
+        return "application/octet-stream"
+    }
+    
+    func documentWasSelected (url: URL) {
+        var error: NSError?
+        
+        NSFileCoordinator().coordinate(
+            readingItemAt: url,
+            options: [],
+            error: &error
+        ) { newURL in
+            let maybeData = try? Data(contentsOf: newURL, options: [])
+            
+            guard let data = maybeData else {
+                self.sendError("Failed to fetch data.")
+                return
+            }
+            do {
                 self.send("start")
                 let mediaType = self.detectMimeType(newURL)
                 var config = ["mediaType": mediaType];
                 if mediaType.contains("video") {
-                     let avAsset = AVAsset(url: newURL)
+                    let avAsset = AVAsset(url: newURL)
                     //生成视频截图
                     let generator = AVAssetImageGenerator(asset: avAsset)
                     generator.appliesPreferredTrackTransform = true
@@ -92,83 +104,83 @@ class Chooser : CDVPlugin {
                     self.send(Data(bytes: bytes[start...start + count - 1]))
                     len = len - count
                 } while(len > 0)
-
-				newURL.stopAccessingSecurityScopedResource()
+                
+                newURL.stopAccessingSecurityScopedResource()
                 self.send("end")
-			}
-			catch let error {
-				self.sendError(error.localizedDescription)
-			}
-		}
-
-		if let error = error {
-			self.sendError(error.localizedDescription)
-		}
-
-		url.stopAccessingSecurityScopedResource()
-	}
-
-	@objc(getFile:)
-	func getFile (command: CDVInvokedUrlCommand) {
-		self.commandCallback = command.callbackId
-
-		let accept = command.arguments.first as! String
-		let mimeTypes = accept.components(separatedBy: ",")
-
-		let utis = mimeTypes.map { (mimeType: String) -> String in
-			switch mimeType {
-				case "audio/*":
-					return kUTTypeAudio as String
-				case "font/*":
-					return "public.font"
-				case "image/*":
-					return kUTTypeImage as String
-				case "text/*":
-					return kUTTypeText as String
-				case "video/*":
-					return kUTTypeVideo as String
-				default:
-					break
-			}
-
-			if mimeType.range(of: "*") == nil {
-				let utiUnmanaged = UTTypeCreatePreferredIdentifierForTag(
-					kUTTagClassMIMEType,
-					mimeType as CFString,
-					nil
-				)
-
+            }
+            catch let error {
+                self.sendError(error.localizedDescription)
+            }
+        }
+        
+        if let error = error {
+            self.sendError(error.localizedDescription)
+        }
+        
+        url.stopAccessingSecurityScopedResource()
+    }
+    
+    @objc(getFile:)
+    func getFile (command: CDVInvokedUrlCommand) {
+        self.commandCallback = command.callbackId
+        
+        let accept = command.arguments.first as! String
+        let mimeTypes = accept.components(separatedBy: ",")
+        
+        let utis = mimeTypes.map { (mimeType: String) -> String in
+            switch mimeType {
+            case "audio/*":
+                return kUTTypeAudio as String
+            case "font/*":
+                return "public.font"
+            case "image/*":
+                return kUTTypeImage as String
+            case "text/*":
+                return kUTTypeText as String
+            case "video/*":
+                return kUTTypeVideo as String
+            default:
+                break
+            }
+            
+            if mimeType.range(of: "*") == nil {
+                let utiUnmanaged = UTTypeCreatePreferredIdentifierForTag(
+                    kUTTagClassMIMEType,
+                    mimeType as CFString,
+                    nil
+                )
+                
                 if let uti = (utiUnmanaged?.takeRetainedValue() as String?) {
-					if !uti.hasPrefix("dyn.") {
-						return uti
-					}
-				}
-			}
-
-			return kUTTypeData as String
-		}
-
-		self.callPicker(utis: utis)
-	}
-
-	func send (_ message: String, _ status: CDVCommandStatus = CDVCommandStatus_OK, _ final: Bool = false) {
-		if let callbackId = self.commandCallback {
-			if(final) {
-				self.commandCallback = nil
-			}
-
-			let pluginResult = CDVPluginResult(
-				status: status,
-				messageAs: message
-			)
+                    if !uti.hasPrefix("dyn.") {
+                        return uti
+                    }
+                }
+            }
+            
+            return kUTTypeData as String
+        }
+        
+        self.callPicker(utis: utis)
+    }
+    
+    func send (_ message: String, _ status: CDVCommandStatus = CDVCommandStatus_OK, _ final: Bool = false) {
+        if let callbackId = self.commandCallback {
+            if(final) {
+                self.commandCallback = nil
+            }
+            
+            let pluginResult = CDVPluginResult(
+                status: status,
+                messageAs: message
+            )
             pluginResult?.setKeepCallbackAs(true)
-
-			self.commandDelegate!.send(
-				pluginResult,
-				callbackId: callbackId
-			)
-		}
-	}
+            
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: callbackId
+            )
+        }
+    }
     
     func send (_ buffer: Data!) {
         if let callbackId = self.commandCallback {
@@ -184,10 +196,10 @@ class Chooser : CDVPlugin {
             )
         }
     }
-
-	func sendError (_ message: String) {
-		self.send(message, CDVCommandStatus_ERROR, true)
-	}
+    
+    func sendError (_ message: String) {
+        self.send(message, CDVCommandStatus_ERROR, true)
+    }
     
     func getThumbnail(_ image: UIImage) -> String {
         var res: String?;
@@ -202,25 +214,27 @@ class Chooser : CDVPlugin {
     }
 }
 
-extension Chooser : UIDocumentPickerDelegate {
-	@available(iOS 11.0, *)
-	func documentPicker (
-		_ controller: UIDocumentPickerViewController,
-		didPickDocumentsAt urls: [URL]
-	) {
-		if let url = urls.first {
-			self.documentWasSelected(url: url)
-		}
-	}
-
-	func documentPicker (
-		_ controller: UIDocumentPickerViewController,
-		didPickDocumentAt url: URL
-	) {
-		self.documentWasSelected(url: url)
-	}
-
-	func documentPickerWasCancelled (_ controller: UIDocumentPickerViewController) {
-		self.send("RESULT_CANCELED")
-	}
+extension Chooser : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.send("RESULT_CANCELED")
+    }
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        var url: URL?
+        if let type = info[UIImagePickerControllerMediaType] as? String {
+            if type == "public.image" {
+                if #available(iOS 11.0, *) {
+                    url = info[UIImagePickerControllerImageURL] as? URL
+                } else {
+                    url = info[UIImagePickerControllerReferenceURL] as? URL
+                }
+            } else {
+                url = info[UIImagePickerControllerMediaType] as? URL
+            }
+        }
+        if let url_ = url {
+            self.documentWasSelected(url: url_)
+            self.viewController.dismiss(animated: true, completion: {})
+        }
+    }
 }
