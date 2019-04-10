6d789550142f61d3a96b7bcbbc4c55673c3ae4a1
1424667164 <1424667164@qq.com>
2019-04-10 23:07:58 +0800

    修改ios插件

--- src/ios/Chooser.swift
+++ src/ios/Chooser.swift
@@ -2,6 +2,7 @@ import UIKit
 import MobileCoreServices
 import AVFoundation
 import Foundation
+import Photos
 
 
 @objc(Chooser)
@@ -10,23 +11,33 @@ class Chooser : CDVPlugin {
     
     func callPicker ( utis: [String]) {
         if !UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            self.send("RESULT_CANCELED");
+            self.send("RESULT_CANCELED")
             return;
         }
-        let imagePicker = UIImagePickerController()
-        //设置代理
-        imagePicker.delegate = self
-        //指定图片控制器类型
-        imagePicker.sourceType = .photoLibrary
-        //只显示视频类型的文件
-        var uts = utis
-        uts.append(kUTTypeMovie as String)
-//        imagePicker.mediaTypes = utis
-        imagePicker.mediaTypes = ["public.image"] // ["public.image", "public.movie"] // ["public.movie"] mpeg-4
-        //不需要编辑
-        imagePicker.allowsEditing = false
-        //弹出控制器，显示界面
-        self.viewController.present(imagePicker, animated: true, completion: nil)
+        self.checkPermission() { hasPerm in
+            if(!hasPerm) {
+                self.sendError("ERROR_NOPERM")
+                return
+            }
+            let imagePicker = UIImagePickerController()
+            //设置代理
+            imagePicker.delegate = self
+            //指定图片控制器类型
+            imagePicker.sourceType = .photoLibrary
+            //只显示视频类型的文件
+            var uts = utis
+            uts.append(kUTTypeMovie as String)
+            imagePicker.mediaTypes = ["public.image", "public.movie"] // ["public.movie"] mpeg-4
+            //不需要编辑
+            imagePicker.allowsEditing = false
+            if #available(iOS 11.0, *) {
+                imagePicker.videoExportPreset = AVAssetExportPreset1920x1080
+            } else {
+                imagePicker.videoQuality = .typeHigh
+            }
+            //弹出控制器，显示界面
+            self.viewController.present(imagePicker, animated: true, completion: nil)
+        }
     }
     
     func detectMimeType (_ url: URL) -> String {
@@ -55,7 +66,7 @@ class Chooser : CDVPlugin {
             error: &error
         ) { newURL in
             let maybeData = try? Data(contentsOf: newURL, options: [])
-            guard let data = maybeData else {
+            guard var data = maybeData else {
                 self.sendError("Failed to fetch data.")
                 return
             }
@@ -64,6 +75,11 @@ class Chooser : CDVPlugin {
                 let mediaType = self.detectMimeType(newURL)
                 var config = ["mediaType": mediaType];
                 if mediaType.contains("video") {
+                    guard let data_ = maybeData else {
+                        self.sendError("Failed to fetch data.")
+                        return
+                    }
+                    data = data_
                     let avAsset = AVAsset(url: newURL)
                     //生成视频截图
                     let generator = AVAssetImageGenerator(asset: avAsset)
@@ -74,7 +90,8 @@ class Chooser : CDVPlugin {
                     config["w"] = String(image.width)
                     config["h"] = String(image.height)
                     config["thumbnail"] = self.getThumbnail(UIImage(cgImage: image))
-                    config["duration"] = String(Int(CMTimeGetSeconds(avAsset.duration)))
+                    config["duration"] = String(Int(CMTimeGetSeconds(avAsset.duration)) * 1000)
+                    config["mediaType"] = "video/mp4"
                 } else if let image = UIImage(data: data) {
                     config["w"] = String(Int(image.size.width))
                     config["h"] = String(Int(image.size.height))
@@ -216,29 +233,6 @@ class Chooser : CDVPlugin {
     }
 }
 
-extension Chooser : UIDocumentPickerDelegate {
-//    @available(iOS 11.0, *)
-//    func documentPicker (
-//        _ controller: UIDocumentPickerViewController,
-//        didPickDocumentsAt urls: [URL]
-//        ) {
-//        if let url = urls.first {
-//            self.documentWasSelected(url: url)
-//        }
-//    }
-//
-//    func documentPicker (
-//        _ controller: UIDocumentPickerViewController,
-//        didPickDocumentAt url: URL
-//        ) {
-//        self.documentWasSelected(url: url)
-//    }
-//
-//    func documentPickerWasCancelled (_ controller: UIDocumentPickerViewController) {
-//        self.send("RESULT_CANCELED")
-//    }
-}
-
 extension Chooser : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.send("RESULT_CANCELED")
@@ -247,6 +241,7 @@ extension Chooser : UIImagePickerControllerDelegate, UINavigationControllerDeleg
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [String : Any]) {
         var url: URL?
+        var isVideo = false
         if let type = info[UIImagePickerControllerMediaType] as? String {
             if type == "public.image" {
                 if #available(iOS 11.0, *) {
@@ -255,14 +250,66 @@ extension Chooser : UIImagePickerControllerDelegate, UINavigationControllerDeleg
                     url = info[UIImagePickerControllerReferenceURL] as? URL
                 }
             } else {
+                isVideo = true
                 url = info[UIImagePickerControllerMediaURL] as? URL
             }
         }
         if let url_ = url {
-            self.documentWasSelected(url: url_)
-            self.viewController.dismiss(animated: true, completion: {})
+            if isVideo {
+                self.mov2mp4(sourceUrl: url_)
+            } else {
+                self.documentWasSelected(url: url_)
+            }
         } else {
-            self.viewController.dismiss(animated: true, completion: {})
+            self.sendError("ERROR_RESOUCE")
         }
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    func checkPermission(callback: @escaping (Bool) -> Void) {
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+        switch photoAuthorizationStatus {
+        case .authorized: callback(true)
+        case .notDetermined: PHPhotoLibrary.requestAuthorization({
+            (newStatus) in
+                if newStatus == PHAuthorizationStatus.authorized {
+                    callback(true)
+                } else {
+                    callback(false)
+                }
+            })
+        case .restricted: callback(false)
+        case .denied: callback(false)
+        }
+    }
+}
+
+extension Chooser {
+    //视频转换格式.mov 转成 .mp4
+    //方法中sourceUrl参数为.mov的URL数据
+    func mov2mp4(sourceUrl: URL) {
+        //以当前时间来为文件命名
+        let date = Date()
+        let formatter = DateFormatter.init()
+        formatter.dateFormat = "yyyyMMddHHmmss"
+        let fileName = formatter.string(from: date) + ".mp4"
+        // 保存在 tmp 地址中，app启动d时会自动删除
+        let docPath = NSTemporaryDirectory()
+        let videoSandBoxPath = (docPath as String) + "ablumVideo" + fileName
+        
+        //转码配置
+        let avAsset = AVAsset(url: sourceUrl)
+        let exportSession = AVAssetExportSession.init(asset: avAsset, presetName: AVAssetExportPreset1920x1080)
+        exportSession?.shouldOptimizeForNetworkUse = true
+        exportSession?.outputURL = URL.init(fileURLWithPath: videoSandBoxPath)
+        exportSession?.outputFileType = AVFileTypeMPEG4 //控制转码的格式
+        exportSession?.exportAsynchronously(completionHandler: {
+            if exportSession?.status == AVAssetExportSessionStatus.failed {
+                self.sendError("ERROR_MP4")
+            }
+            if exportSession?.status == AVAssetExportSessionStatus.completed {
+                self.documentWasSelected(url: URL.init(fileURLWithPath: videoSandBoxPath))
+            }
+        })
     }
 }
